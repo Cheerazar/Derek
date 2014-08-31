b5acf8bf2b40a91aef86ce88d73ab33516e98798
Gunnari Auvinen & <pair+cheerazar.reticulin@devbootcamp.com>
2014-08-31 15:30:33 -0700

    Add Teacher and User models. Update create_teachers and create_signups migrations.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,4 @@
 class Teacher < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :session, foreign_key: :session_id, class_name: :Breakout
   belongs_to :instructor, :class_name => 'User'
 end
--- db/migrate/20140831192557_create_signups.rb
+++ db/migrate/20140831192557_create_signups.rb
@@ -2,7 +2,7 @@ class CreateSignups < ActiveRecord::Migration
   def change
     create_table :signups do |t|
       t.integer       :user_id
-      t.breakout_id   :breakout_id
+      t.integer       :breakout_id
     end
   end
 end
