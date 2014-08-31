80ffac383f14c053eccd687069d666ff60263687
Gunnari Auvinen & <pair+cheerazar.reticulin@devbootcamp.com>
2014-08-31 12:32:04 -0700

    Add migrations for breakouts, comments, teachers, and signups.

---
+++ db/migrate/20140831184645_create_breakouts.rb
@@ -0,0 +1,13 @@
+class CreateBreakouts < ActiveRecord::Migration
+  def change
+    create_table :breakouts do |t|
+      t.string :category
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.integer :teacher_id
+      t.string :location
+      t.timestamps
+    end
+  end
+end
