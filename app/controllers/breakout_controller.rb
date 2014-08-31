cf58e5c304cc45c50237d07f09e88dd8b9f0dfd5
Matt Chan & Yen Tran <pair+matt16749.yenhtran@devbootcamp.com>
2014-08-31 15:25:19 -0700

    adds nav-bar with Bootstrap adds header partial adds initial breakout controller actions adds partials for BootstrapCDN generates devise views

--- app/controllers/breakout_controller.rb
+++ app/controllers/breakout_controller.rb
@@ -1,6 +1,27 @@
 class BreakoutController < ApplicationController
   def index
+  end
+
+  def new
+  end
+
+  def edit
 
   end
 
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+    breakout_id = params[:id]
+    breakout = Breakout.find(breakout_id)
+    breakout.destroy
+    redirect_to user_breakouts_path
+  end
 end
+
+
+
